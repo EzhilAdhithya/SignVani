74ee5004ebd2a66983dd8ed346cf55730229e039
Sri-Krishna-V <srikrishna.vundavalli@gmail.com>
2026-02-12 18:30:55 +0530

    feat: add wordLoader utility for animation handling
    - Introduced wordLoader.js to manage word animations from JSON data.
    - Implemented functions to load individual and all word animations.
    - Added expression evaluation for Math.PI to enhance animation transformations.
    - Included utility to retrieve available words and descriptions from JSON.

--- client/src/Animations/words.js
+++ client/src/Animations/words.js
@@ -1,10 +1,31 @@
-import { TIME } from './Words/TIME';
-import { HOME } from './Words/HOME';
-import { PERSON } from './Words/PERSON';
-import { YOU } from './Words/YOU';
+// JSON-based word loading system
+// All word animations are now loaded from wordsData.json
+import { loadAllWords, getAvailableWordsFromJSON } from './Utils/wordLoader';
 
-var wordList = ['TIME', 'HOME', 'PERSON', 'YOU'];
+// Load all words from JSON
+const loadedWords = loadAllWords();
 
-export {
-    TIME, HOME, PERSON, YOU, wordList
-}
+// Export individual word animation functions (for backward compatibility)
+export const TIME = loadedWords.TIME;
+export const HOME = loadedWords.HOME;
+export const PERSON = loadedWords.PERSON;
+export const YOU = loadedWords.YOU;
+export const HELLO = loadedWords.HELLO;
+export const THANK = loadedWords.THANK;
+export const PLEASE = loadedWords.PLEASE;
+export const SORRY = loadedWords.SORRY;
+export const YES = loadedWords.YES;
+export const NO = loadedWords.NO;
+export const HELP = loadedWords.HELP;
+export const NAME = loadedWords.NAME;
+export const FAMILY = loadedWords.FAMILY;
+export const FRIEND = loadedWords.FRIEND;
+export const WORK = loadedWords.WORK;
+export const SCHOOL = loadedWords.SCHOOL;
+export const EAT = loadedWords.EAT;
+export const DRINK = loadedWords.DRINK;
+export const GOOD = loadedWords.GOOD;
+export const BAD = loadedWords.BAD;
+
+// Export wordList (automatically generated from JSON)
+export const wordList = getAvailableWordsFromJSON();
